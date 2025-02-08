a342aed2ec4566e99197964704c339c158ed0f24
2004khushig <143532781+2004khushig@users.noreply.github.com>
2025-02-08 18:46:37 +0530

    Time: 7 ms (77.91%) | Memory: 44.8 MB (51.82%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
@@ -1,24 +1,29 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        int a=word1.length();
-        int b=word2.length();
-        int[][]dp=new int[a+1][b+1];
-        for(int i=0;i<=a;i++){
-            for(int j=0;j<=b;j++){
-                if(i==0||j==0){
-                    dp[i][j]=0;
+        int m = word1.length();
+        int n = word2.length();
+
+        int[][] dp = new int[m + 1][n + 1];
+
+        for(int i = 0; i <= m; i++) {
+            for(int j = 0; j <= n; j++) {
+                if(i == 0 || j == 0) {
+                    dp[i][j] = 0;
                 }
-                else if(word1.charAt(i-1)==word2.charAt(j-1)){
-                    dp[i][j]=1+dp[i-1][j-1];
+                else if(word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
                 }
-                else{
-                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
         }
-        int res=dp[a][b];
-        int del=a-res;
-        int insert=b-res;
-        return del+insert;
+
+        int res = dp[m][n];
+
+        int deletion = m - res;
+        int insertion = n - res;
+
+        return deletion + insertion;
     }
 }
