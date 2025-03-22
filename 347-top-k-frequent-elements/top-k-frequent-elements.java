6821335d298961b6f4664fa9065c2864acf1348d
2004khushig <143532781+2004khushig@users.noreply.github.com>
2025-03-22 19:58:12 +0530

    Time: 13 ms (76.67%) | Memory: 49.5 MB (6.58%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> freqMap = new HashMap<>();
+        for (int num : nums) {
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        }
+
+        List<Integer>[] buckets = new ArrayList[nums.length + 1];
+        for (var entry : freqMap.entrySet()) {
+            int freq = entry.getValue();
+            if (buckets[freq] == null) {
+                buckets[freq] = new ArrayList<>();
+            }
+            buckets[freq].add(entry.getKey());
+        }
+
+        List<Integer> result = new ArrayList<>();
+        for (int i = nums.length; i > 0 && result.size() < k; i--) {
+            if (buckets[i] != null) {
+                result.addAll(buckets[i]);
+            }
+        }
+
+        return result.stream().mapToInt(i -> i).toArray();
+    }
+}
