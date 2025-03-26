83ce10c867cfdc64d0f8de147650097563d7a905
Khushi Gupta <143532781+2004khushig@users.noreply.github.com>
2025-03-26 17:23:30 +0530

    Time: 2 ms (83.03%) | Memory: 42.8 MB (27.94%) - LeetSync

---
+++ 862-find-and-replace-in-string/find-and-replace-in-string.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String findReplaceString(String s, int[] indices, String[] sources, String[] targets) {
+        Map<Integer,Integer>map=new HashMap<>();
+        for(int i=0;i<indices.length;i++){
+            if(s.startsWith(sources[i],indices[i])){
+                map.put(indices[i],i);
+            }
+        }
+        StringBuilder sb=new StringBuilder();
+        int i=0;
+        while (i < s.length()) {
+            if (!map.containsKey(i)) { 
+                sb.append(s.charAt(i));
+                i++;
+            } else { 
+                sb.append(targets[map.get(i)]); 
+                i += sources[map.get(i)].length(); 
+            }
+        }
+
+        return sb.toString();
+    }
+}
