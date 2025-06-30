b09a01b7994ea02f247b8124c221c5ea7b52b8ed
Khushi Gupta <143532781+2004khushig@users.noreply.github.com>
2025-06-30 10:41:27 +0530

    Time: 1 ms (71.54%) | Memory: 42.1 MB (61.72%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -1,35 +1,41 @@
-import java.util.*;
-
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> result = new ArrayList<>();
-        if (root == null) {
-            return result;
+        List<Integer>arr=new ArrayList<>();
+        if(root==null){
+            return arr;
         }
-
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-
-        while (!queue.isEmpty()) {
-            int levelSize = queue.size();
-
-            for (int i = 0; i < levelSize; i++) {
-                TreeNode currNode = queue.poll();
-                
-                // Add the last element of each level to the result list
-                if (i == levelSize - 1) {
-                    result.add(currNode.val);
+        Queue<TreeNode>q=new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()){
+            int size=q.size();
+            for(int i=0;i<size;i++){
+                TreeNode curr=q.poll();
+                if(i==size-1){
+                    arr.add(curr.val);
                 }
-
-                if (currNode.left != null) {
-                    queue.offer(currNode.left);
+                if(curr.left!=null){
+                    q.add(curr.left);
                 }
-                if (currNode.right != null) {
-                    queue.offer(currNode.right);
+                if(curr.right!=null){
+                    q.add(curr.right);
                 }
             }
         }
-
-        return result;
+        return arr;
     }
 }
