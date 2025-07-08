f8bee2e54abf94f4129d785004ad18ac09eae6b2
Khushi Gupta <143532781+2004khushig@users.noreply.github.com>
2025-07-08 19:08:21 +0530

    Time: 2 ms (92.15%) | Memory: 41.8 MB (37.26%) - LeetSync

--- 38-count-and-say/count-and-say.java
+++ 38-count-and-say/count-and-say.java
@@ -3,20 +3,20 @@ class Solution {
         if(n==1){
             return "1";
         }
-        String p=countAndSay(n-1);
-        int q=p.length();
+        String a=countAndSay(n-1);
+        int b=a.length();
         StringBuilder sb=new StringBuilder();
         int count=1;
-        for(int i=1;i<q;i++){
-            if(p.charAt(i)==p.charAt(i-1)){
+        for(int i=1;i<b;i++){
+            if(a.charAt(i)==a.charAt(i-1)){
                 count++;
             }
             else{
-                sb.append(count).append(p.charAt(i-1));
+                sb.append(count).append(a.charAt(i-1));
                 count=1;
             }
         }
-        sb.append(count).append(p.charAt(q-1));
+        sb.append(count).append(a.charAt(b-1));
         return sb.toString();
     }
 }
