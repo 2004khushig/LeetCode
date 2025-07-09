1658c3affcd515a01e352ab5fb59e4b62ccedd7d
Khushi Gupta <143532781+2004khushig@users.noreply.github.com>
2025-07-09 16:26:21 +0530

    Time: 2 ms (55.16%) | Memory: 46.1 MB (60.55%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,38 +1,25 @@
 class Solution {
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        // Ensure nums1 is the smaller array
-        if (nums1.length > nums2.length) {
-            return findMedianSortedArrays(nums2, nums1);
-        }
-
-        int x = nums1.length;
-        int y = nums2.length;
-
-        int low = 0, high = x;
-        while (low <= high) {
-            int partitionX = (low + high) / 2;
-            int partitionY = (x + y + 1) / 2 - partitionX;
-
-            int maxLeftX = (partitionX == 0) ? Integer.MIN_VALUE : nums1[partitionX - 1];
-            int minRightX = (partitionX == x) ? Integer.MAX_VALUE : nums1[partitionX];
-
-            int maxLeftY = (partitionY == 0) ? Integer.MIN_VALUE : nums2[partitionY - 1];
-            int minRightY = (partitionY == y) ? Integer.MAX_VALUE : nums2[partitionY];
-
-            if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
-                // Found the correct partition
-                if ((x + y) % 2 == 0) {
-                    return (Math.max(maxLeftX, maxLeftY) + Math.min(minRightX, minRightY)) / 2.0;
-                } else {
-                    return Math.max(maxLeftX, maxLeftY);
-                }
-            } else if (maxLeftX > minRightY) {
-                high = partitionX - 1;
-            } else {
-                low = partitionX + 1;
+    public double findMedianSortedArrays(int[] num1, int[] num2) {
+        int m=num1.length;
+        int n=num2.length;
+        int[]merge=new int[m+n];
+        int i=0,j=0,k=0;
+        while(i<m && j<n){
+            if(num1[i]<num2[j]){
+                merge[k++]=num1[i++];
+            }
+            else{
+                merge[k++]=num2[j++];
             }
         }
-
-        throw new IllegalArgumentException("Input arrays are not sorted or valid.");
+        while(i<m) merge[k++]=num1[i++];
+        while(j<n) merge[k++]=num2[j++];
+        int total=m+n;
+        if(total%2==0){
+            return (merge[total/2-1]+merge[total/2])/2.0;
+        }
+        else{
+            return merge[total/2];
+        }
     }
 }
