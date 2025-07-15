672af336690b158e1d3353bd1eda03576008cf33
Khushi Gupta <143532781+2004khushig@users.noreply.github.com>
2025-07-15 20:33:54 +0530

    Time: 140 ms (60.95%) | Memory: 42 MB (29.31%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,42 +1,28 @@
-public class Solution {
+class Solution {
     public boolean exist(char[][] board, String word) {
-        int m = board.length;
-        int n = board[0].length;
-
-        boolean[][] visited = new boolean[m][n];
-        boolean result = false;
-        
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (board[i][j] == word.charAt(0)) {
-                    result = backtrack(board, word, visited, i, j, 0);
-                    if (result) return true;
+        int m=board.length;
+        int n=board[0].length;
+        boolean[][]visited=new boolean[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(dfs(board,word,i,j,0,visited)){
+                    return true;
                 }
             }
         }
-        
         return false;
+        
     }
-    
-    private boolean backtrack(char[][] board, String word, boolean[][] visited, int i, int j, int index) {
-        if (index == word.length()) {
+    public static boolean dfs(char[][]board,String word,int i,int j,int index,boolean[][]visited){
+        if(index==word.length()){
             return true;
         }
-        
-        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || visited[i][j] || board[i][j] != word.charAt(index)) {
+        if(i<0||j<0||i>=board.length||j>=board[0].length||visited[i][j]||board[i][j]!=word.charAt(index)){
             return false;
         }
-        
-        visited[i][j] = true;
-        
-        if (backtrack(board, word, visited, i + 1, j, index + 1) ||
-            backtrack(board, word, visited, i - 1, j, index + 1) ||
-            backtrack(board, word, visited, i, j + 1, index + 1) ||
-            backtrack(board, word, visited, i, j - 1, index + 1)) {
-            return true;
-        }
-        
-        visited[i][j] = false;
-        return false;
+        visited[i][j]=true;
+        boolean found=dfs(board,word,i+1,j,index+1,visited)||dfs(board,word,i-1,j,index+1,visited)||dfs(board,word,i,j+1,index+1,visited)||dfs(board,word,i,j-1,index+1,visited);
+        visited[i][j]=false;
+        return found;
     }
 }
