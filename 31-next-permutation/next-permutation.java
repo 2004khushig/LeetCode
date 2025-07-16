aafe77fe70a8fa633c5340f18415471e3a8c65c0
Khushi Gupta <143532781+2004khushig@users.noreply.github.com>
2025-07-16 15:58:46 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (88.40%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,35 +1,27 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int n=nums.length;
-        int pivot=-1;
-        for(int i=n-1;i>=1;i--){
-            if(nums[i]>nums[i-1]){
-                pivot=i-1;
-                break;
+         int n=nums.length;
+        int i=n-2;
+        while(i>=0 && nums[i]>=nums[i+1]){
+            i--;
+        }
+        if(i>=0){
+            int j=n-1;
+            while(nums[j]<=nums[i]){
+                j--;
             }
+            swap(nums,i,j);
         }
-        if(pivot==-1){
-            reverse(nums,0,n-1);
-            return;
-        }
-        for(int i=n-1;i>pivot;i--){
-            if(nums[i]>nums[pivot]){
-                int temp=nums[i];
-                nums[i]=nums[pivot];
-                nums[pivot]=temp;
-                break;
-            }
-        }
-        reverse(nums,pivot+1,n-1);
-        
+        reverse(nums,i+1,n-1);
     }
-    public void reverse(int nums[],int s,int e){
-        while(s<e){
-            int swap = nums[s];
-            nums[s] =  nums[e];
-            nums[e] = swap;
-            s++;
-            e--; 
+    public static void swap(int[]nums,int i,int j){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    }
+    public static void reverse(int[]nums,int i,int j){
+        while(i<j){
+            swap(nums,i++,j--);
         }
     }
 }
