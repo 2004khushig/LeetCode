61a118c66e1f05730da0debf3762bf80b6979228
Khushi Gupta <143532781+2004khushig@users.noreply.github.com>
2025-08-26 22:10:26 +0530

    Time: 74 ms (44.60%) | Memory: 57.8 MB (35.68%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -4,7 +4,7 @@ class Solution {
         s.push(-1);
         int area=0;
         for(int i=0;i<heights.length;i++){
-            while(s.peek()!=-1 && heights[i]<=heights[s.peek()]){
+            while(s.peek()!=-1 && heights[i]<heights[s.peek()]){
                 int h=heights[s.pop()];
                 int w=i-s.peek()-1;
                 area=Math.max(area,h*w);
@@ -17,5 +17,6 @@ class Solution {
             area=Math.max(area,h*w);
         }
         return area;
+
     }
 }
