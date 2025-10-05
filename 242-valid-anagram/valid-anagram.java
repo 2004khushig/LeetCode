dcba1d68fa94bbe207e16a893d60b0e29481b63e
Khushi Gupta <143532781+2004khushig@users.noreply.github.com>
2025-10-05 13:20:50 +0530

    Time: 17 ms (17.20%) | Memory: 44.7 MB (43.44%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -3,15 +3,17 @@ class Solution {
         if(s.length()!=t.length()){
             return false;
         }
-        HashMap<Character,Integer>hm=new HashMap<>();
+        HashMap<Character, Integer>hm=new HashMap<>();
         for(int i=0;i<s.length();i++){
-            hm.put(s.charAt(i),hm.getOrDefault(s.charAt(i),0)+1);
+            char a=s.charAt(i);
+            hm.put(a,hm.getOrDefault(a,0)+1);
         }
         for(int i=0;i<t.length();i++){
-            if (!hm.containsKey(t.charAt(i)) || hm.get(t.charAt(i)) == 0) {
+            char a=t.charAt(i);
+            if(!hm.containsKey(a)||hm.get(a)==0){
                 return false;
             }
-            hm.put(t.charAt(i),hm.get(t.charAt(i))-1);
+            hm.put(a,hm.getOrDefault(a,0)-1);
         }
         return true;
     }
