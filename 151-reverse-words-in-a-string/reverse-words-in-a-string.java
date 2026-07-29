b2ff6eba999384ab27a46909fcb17c933300e714
Khushi Gupta <143532781+2004khushig@users.noreply.github.com>
2026-07-29 21:20:33 +0530

    Time: 7 ms (63.66%) | Memory: 44.5 MB (46.86%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -5,7 +5,7 @@ class Solution {
         for(int i=words.length-1;i>=0;i--){
             ans.append(words[i]);
             if(i!=0){
-            ans.append(" ");
+                ans.append(" ");
             }
         }
         return ans.toString().trim();
